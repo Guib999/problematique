ecd739e0b37f48379de211c445c2acff092d10fb
Guib999 <laroucheg12@gmail.com>
2025-09-24 11:22:17 -0400

    fini

--- banc_lidar.py
+++ banc_lidar.py
@@ -13,8 +13,9 @@ Date: jj/mm/aaaa
 """ 
 
 import sys
+import filtrage
 
-from os import *
+
 
 #===========================================
 # Méthode(s) à compléter...
@@ -24,17 +25,20 @@ def lire():
     #print(getcwd())
     nom="exemples/donnees_test.txt"
     point=[]
-    try:
-        doc = open(nom, "r")
-        contenu = "a"
-        while contenu !="":
-            contenu = int(doc.readline())
-            point.append(contenu)
-
+    #try:
+    doc = open(nom, "r")
+    contenu = "a"
+    while contenu !="":
+        contenu = doc.readline()
+        if contenu !="":
+            p = float(contenu[0:len(contenu)-1])
         
-        doc.close()
-    except Exception as e:
-        print("erreur: ", e)
+            point.append(p)
+
+    
+    doc.close()
+    #except Exception as e:
+     #   print("erreur: ", e)
     
     return point
     
@@ -44,12 +48,26 @@ def lire():
     
 
 def decide_filtre():
-    choix = input("Quel filtre vouler vous?\n" \
-    "Écrire 1 pour faire un filtre de moyenne.\n" \
-    "Écrire 2 pour un filtre médiane.\n",)
+    bonChoix= False
+    while bonChoix==False:
+        choix = input("Quel filtre vouler vous?\n" \
+        "Écrire 1 pour faire un filtre de moyenne.\n" \
+        "Écrire 2 pour un filtre médiane.\n"
+        "Écrire 3 pour un filtre min et max\n"\
+        "Écrire 4 pour arrêter le programme\n")
+        if choix == '1' or choix=='2' or choix == '3' or choix =='4':
+            bonChoix = True
+        else :
+            print("choix mal écrit")
 
     return choix
 
+def ecrire(points):
+    doc = open("exemples/donnees_final", 'w')
+    for p in points:
+        doc.write(str(p)+"\n")
+    doc.close()
+    
 
 
 #===========================================
@@ -58,6 +76,29 @@ if __name__ == "__main__":
     DESC: Point d'entrée du programme
     """
     print("Test lidar (GRO120)")
-    #decide_filtre()
-    lire()
+    continu = True
+    points=lire()
+    while continu == True:
+        choix =decide_filtre()
+        if choix == '1':
+            points = filtrage.filtre_moyenne(points)
+        elif choix=='2':
+            points = filtrage.filtre_mediane(points)
+        elif choix =='3':
+            points=filtrage.filtre_min_max(points)
+        else:
+            continu = False
+            tmp = filtrage.filtre_bonne_valeur(points)
+            total = 0
+            for p in tmp:
+                total = total+p
+            print("moyenne:", round(total/len(tmp),1))
+            print("Bonne valeur:", len(tmp))
+            print("valeur total:", len(points))
+            ecrire(points)
+                
+
+        
+
+    
     
